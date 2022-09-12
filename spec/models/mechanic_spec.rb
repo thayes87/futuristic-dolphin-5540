7e63ea66915ffd5c007cf61d07ca2c1b3d0cb951
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-12 10:14:14 -0600

    model testing for Mechanic

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  describe "class methods" do
+    describe "#self.average_years_of_experience" do
+      it 'returns average years of experience for mechanics' do
+        tom = Mechanic.create!(name: "Tom", years_experience: 10)
+        zach = Mechanic.create!(name: "Zach", years_experience: 9)
+        alex = Mechanic.create!(name: "Alex", years_experience: 8)
+
+        expect(Mechanic.average_years_of_experience).to eq(9)
+      end
+    end
+  end
 end
