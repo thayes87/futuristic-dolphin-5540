b0bc922d60c7b588192c0029ea4b4572e081c7ef
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-12 10:11:13 -0600

    complete user story one- add average_years_of_expereince to Mechanic model

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'the mechanics index page' do
   describe 'As a user, When I visit the mechanics index page' do
     it "has a list of all mechanics names and their years of expperience" do
-      tom = Mechanic.create!(name: "Tom", years_experience: 11)
+      tom = Mechanic.create!(name: "Tom", years_experience: 10)
       zach = Mechanic.create!(name: "Zach", years_experience: 9)
-      alex = Mechanic.create!(name: "Alex", years_experience: 5)
+      alex = Mechanic.create!(name: "Alex", years_experience: 8)
       
       visit '/mechanics'
 
@@ -13,12 +13,14 @@ RSpec.describe 'the mechanics index page' do
 
       within "#Mechanics" do
         expect(page).to have_content("Name: Tom")
-        expect(page).to have_content("Years of Experience: 11")
+        expect(page).to have_content("Years of Experience: 10")
         expect(page).to have_content("Name: Zach")
         expect(page).to have_content("Years of Experience: 9")
         expect(page).to have_content("Name: Alex")
-        expect(page).to have_content("Years of Experience: 5")
-      end 
+        expect(page).to have_content("Years of Experience: 8")
+      end
+
+      expect(page).to have_content("Average Years of Expereince: 9")
     end
   end
 end 
